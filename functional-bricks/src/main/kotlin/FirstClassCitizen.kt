012ed66645631d8d4b5e6c446e1eb359b8afb17c
braintelligencePL <iluzion5@o2.pl>
2019-03-09 14:37:52 +0100

    first class citizen

---
+++ functional-bricks/src/main/kotlin/FirstClassCitizen.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+
+    // example of first class citizen OR function as a value
+    val adder: (Int, Int) -> Double = { a, b -> (a + b).toDouble() }
+
+    print(adder(1, 1))
+}
