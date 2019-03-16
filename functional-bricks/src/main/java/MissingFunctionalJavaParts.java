535ee26690cd420547e830612f2cfd07279c114e
braintelligencePL <iluzion5@o2.pl>
2019-03-17 00:25:18 +0100

    functional java parts

--- functional-bricks/src/main/java/MissingFunctionalJavaParts.java
+++ functional-bricks/src/main/java/MissingFunctionalJavaParts.java
@@ -30,14 +30,37 @@ public class MissingFunctionalJavaParts {
                 Optional.of("One"),
                 Optional.of("Two")
         ));
-
         System.out.println(legacyWay);
+
 
         // vavr flatMap
         val vavrWay = vavrFlatMap(List.of(
                 Option.of("One"),
                 Option.of("One")
         ));
+        System.out.println(vavrWay);
+
+        // Classical if/else for null
+        System.out.println(
+                nullCheck(null)
+        );
+
+        // Better if/else with Option
+        System.out.println(
+                vavrNullCheck(null)
+        );
+
+
+        // More verbose example for conditions
+        System.out.println(
+                normalConditionChecker("qwerty")
+        );
+
+        // Functional condition check
+        System.out.println(
+                vavrConditionChecker("qwerty")
+        );
+
 
     }
 
@@ -46,7 +69,6 @@ public class MissingFunctionalJavaParts {
                 .of(args)
                 .zipWithIndex((arg, index) -> index + ". " + arg);
     }
-
 
     private static java.util.List<String> legacyFlatMap(java.util.List<Optional<String>> args) {
         return args
@@ -60,5 +82,34 @@ public class MissingFunctionalJavaParts {
         return args
                 .flatMap(Function.identity()) // same as flatMap(arg -> arg)
                 .map(String::toLowerCase);
+    }
+
+    private static String nullCheck(String value) {
+        if (value != null) {
+            return value.toUpperCase();
+        }
+        return value;
+    }
+
+    private static String vavrNullCheck(String value) {
+        return Option.of(value)
+                .map(String::toLowerCase) // executed only when Option is some()
+                .getOrElse(value);
+    }
+
+    private static String normalConditionChecker(String value) {
+        if (value != null && value.startsWith("A") && value.endsWith("Z")) {
+            return "YEYY!!";
+        } else {
+            return "ELSE";
+        }
+    }
+
+    private static String vavrConditionChecker(String value) {
+        return Option.of(value)
+                .filter(given -> given.startsWith("A"))
+                .filter(given -> given.endsWith("Z"))
+                .map(ignore -> "YEYY!!")
+                .getOrElse("ELSE");
     }
 }
